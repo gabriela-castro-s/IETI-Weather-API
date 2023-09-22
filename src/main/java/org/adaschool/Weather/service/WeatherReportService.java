c96ed39ff7564aa5b68abce3e195770a257f20e5
Gabriela Castro <gabriela.castro@mail.escuelaing.edu.co>
2023-09-22 12:40:31 -0500

    Test corriendo

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "aae30fa1ac595c368af30ef182722be1";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
--- src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
+++ src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
@@ -1,13 +1,35 @@
 package org.adaschool.Weather;
 
+import org.adaschool.Weather.controller.WeatherReportController;
+import org.adaschool.Weather.data.WeatherReport;
+import org.adaschool.Weather.service.WeatherReportService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 class WeatherApplicationTests {
+
+	@Mock
+	private WeatherReportController weatherReportController;
+
+	@InjectMocks
+	private WeatherReportService weatherReportService;
+
+	@Mock
+	private WeatherReport weatherReport;
 
 	@Test
 	void contextLoads() {
+		MockitoAnnotations.initMocks(this);
+		when(weatherReportController.getWeatherReport(37.8267, -122.4233)).thenReturn(weatherReport);
+		WeatherReport trialWeather = weatherReportService.getWeatherReport(37.8267, -122.4233);
+		Assert.assertEquals(trialWeather.getTemperature(), weatherReport.getTemperature(), 1);
 	}
 
 }
